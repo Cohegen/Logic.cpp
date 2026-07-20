0ba7261a7e80b7515096ba03580afadb32b4b92a
Antonius <161943308+Cohegen@users.noreply.github.com>
2026-07-20 11:16:07 +0300

    Add Signal class for modeling digital signals

---
+++ Signals/signal.hpp
@@ -0,0 +1,28 @@
+/*
+Modelling a signal--
+From digital design we know that a signal can be either high or low on a wire
+*/
+
+#pragma once
+
+namespace dd
+{
+    class Signal{
+        public:
+        Signal() = default;
+        explicit Signal(bool value): m_value(value){}
+
+        bool value() const
+        {
+            return m_value;
+        }
+
+        void set(bool value)
+        {
+            m_value = value;
+        }
+
+        private:
+        bool m_value{false};
+    }
+};
